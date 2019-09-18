030178af3fe4b0b8b72c2a286d27b2331c71b96d
Hanh Tran <tranduchanh.ms@gmail.com>
2019-09-18 16:06:39 +0900

    Setup simple task

--- main/main.c
+++ main/main.c
@@ -17,8 +17,11 @@
   @brief Entry of application.
 */
 
+#include <stdio.h>
+#include <string.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <esp_system.h>
 #include <esp_log.h>
 
 /* @brief tag used for ESP serial console messages */
@@ -27,17 +30,16 @@ static const char MAIN_TAG[] = "ASN_MAIN";
 
 /**
  * @brief FreeRTOS task that periodically prints the heap memory available.
+ *
  * @note Pure debug information, should not be ever started on production code!
  */
 void monitoring_task(void *pvParameter)
 {
 	for(;;)
   {
-		ESP_LOGI(MAIN_TAG, "free heap: %zu", heap_caps_get_free_size(MALLOC_CAP_8BIT));
-		vTaskDelay(10000 / portTICK_PERIOD_MS);
+		ESP_LOGI(MAIN_TAG, "free heap: %d", esp_get_free_heap_size());
+		vTaskDelay(pdMS_TO_TICKS(10000));
 	}
-  
-  vTaskDelete(NULL);
 }
 
 
@@ -53,7 +55,8 @@ void app_setup()
 void app_main()
 {
   // TODO: Start your main tasks here
-  /* Simply create a task that monitors free heap memory and it is pinned to AVAILABLE CPU */
+
+  /* Simply create a task that monitors free heap memory and it is pinned to APP CPU */
   xTaskCreatePinnedToCore(&monitoring_task, (const char * const)"monitor_task",
-                          configMINIMAL_STACK_SIZE, NULL, 1, NULL, tskNO_AFFINITY);
+                          2048, NULL, 1, NULL, 1);
 }
